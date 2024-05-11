f373009dd90baa3c71c5f33d634cca7cd8ebdf62
William Turner <willtur.will@gmail.com>
2024-05-10 23:52:22 -0400

    Favor gpg-agent instead of spawning an extra ssh-agent

--- profile/.profile.d/20-lua.sh
+++ profile/.profile.d/20-lua.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-if (command -v luarocks > /dev/null); then
+if command -v luarocks > /dev/null; then
   export PATH="$(luarocks path --lr-bin | cut -d: -f1)${PATH:+:${PATH}}"
 fi
--- profile/.profile.d/80-default-apps.sh
+++ profile/.profile.d/80-default-apps.sh
@@ -1,43 +1,43 @@
 #!/usr/bin/env sh
 
 # Select the best available editor
-if (command -v nvim > /dev/null); then
+if command -v nvim > /dev/null; then
   export EDITOR="$(command -v nvim)"
-elif (command -v vim > /dev/null); then
+elif command -v vim > /dev/null; then
   export EDITOR="$(command -v vim)"
-elif (command -v vi > /dev/null); then
+elif command -v vi > /dev/null; then
   export EDITOR="$(command -v vi)"
-elif (command -v nano > /dev/null); then
+elif command -v nano > /dev/null; then
   export EDITOR="$(command -v nano)"
 fi
 
 # Select the best available pager
-if (command -v less > /dev/null); then
+if command -v less > /dev/null; then
   export PAGER="$(command -v less)"
-elif (command -v more > /dev/null); then
+elif command -v more > /dev/null; then
   export PAGER="$(command -v more)"
 fi
 
 # Select the best available terminal, overrides the i3-sensible-terminal selection order
-if (command -v st > /dev/null); then
+if command -v st > /dev/null; then
   export TERMINAL="$(command -v st)"
-elif (command -v kitty > /dev/null); then
+elif command -v kitty > /dev/null; then
   export TERMINAL="$(command -v kitty)"
-elif (command -v alacritty > /dev/null); then
+elif command -v alacritty > /dev/null; then
   export TERMINAL="$(command -v alacritty)"
-elif (command -v urxvt > /dev/null); then
+elif command -v urxvt > /dev/null; then
   export TERMINAL="$(command -v urxvt)"
 fi
 
 # Select the best available browser
-if (command -v google-chrome-beta > /dev/null); then
+if command -v google-chrome-beta > /dev/null; then
   export BROWSER="$(command -v google-chrome-beta)"
-elif (command -v google-chrome-stable > /dev/null); then
+elif command -v google-chrome-stable > /dev/null; then
   export BROWSER="$(command -v google-chrome-stable)"
-elif (command -v chromium-snapshot-bin > /dev/null); then
+elif command -v chromium-snapshot-bin > /dev/null; then
   export BROWSER="$(command -v chromium-snapshot-bin)"
-elif (command -v chromium > /dev/null); then
+elif command -v chromium > /dev/null; then
   export BROWSER="$(command -v chromium)"
-elif (command -v firefox > /dev/null); then
+elif command -v firefox > /dev/null; then
   export BROWSER="$(command -v firefox)"
 fi
