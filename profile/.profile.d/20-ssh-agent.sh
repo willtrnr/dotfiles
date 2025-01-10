2dc25c16eca01c14288df370888847801a7f8d2d
William Turner <willtur.will@gmail.com>
2025-01-09 21:53:46 -0500

    Use ps with grep instead of pgrep for non-standard environment compat

--- profile/.profile.d/20-ssh-agent.sh
+++ profile/.profile.d/20-ssh-agent.sh
@@ -6,7 +6,7 @@ if [ -e "${XDG_RUNTIME_DIR}/gnupg/S.gpg-agent.ssh" ]; then
     export SSH_AGENT_PID=''
   fi
 elif [ -z "${SSH_AUTH_SOCK}" ] && command -v ssh-agent > /dev/null; then
-  if ! (pgrep -u "${USER}" ssh-agent > /dev/null); then
+  if ! (ps -u "${USER}" | grep /ssh-agent > /dev/null); then
     ssh-agent > "${XDG_RUNTIME_DIR}/ssh-agent.env"
   fi
   if [ -e "${XDG_RUNTIME_DIR}/ssh-agent.env" ]; then
