32c4eb1818265b186553c6841a13179897e44a20
William Turner <william.turner@aero.bombardier.com>
2025-03-05 16:14:30 -0500

    Overengineer the wezterm config a bit more

--- wezterm/.config/wezterm/helpers.lua
+++ wezterm/.config/wezterm/helpers.lua
@@ -4,16 +4,72 @@ local M = {}
 
 -- Defaults
 M.dpi = 96
-M.fps = 60
-
--- For Windows shenanigans
-M.is_windows = not not wezterm.target_triple:find("-windows-")
 
 ---Converts pixels to points
 ---@param px number
 ---@return number
 function M.px_to_pt(px)
    return (px / M.dpi) / (1 / 72)
+end
+
+---@generic T
+---@param fn fun(): T
+---@return fun(): T
+function M.memoized(fn)
+   local value = nil
+   return function()
+      if value == nil then
+         value = fn()
+      end
+      return value
+   end
+end
+
+-- For Windows shenanigans
+M.running_on_windows = M.memoized(function()
+   return not not wezterm.target_triple:find("-windows-")
+end)
+
+M.running_in_vm = M.memoized(function()
+   ---@type boolean
+   local value = wezterm.run_child_process({ "systemd-detect-virt", "--vm", "--quiet" })
+   return value
+end)
+
+---@generic T
+---@param cond any
+---@param if_true T
+---@param if_false T
+---@return T
+function M.ternary(cond, if_true, if_false)
+   if cond then
+      return if_true
+   else
+      return if_false
+   end
+end
+
+---@generic T
+---@param value T
+---@param fn fun(value: T)
+---@return T
+function M.doto(value, fn)
+   fn(value)
+   return value
+end
+
+---@generic T
+---@param it T[]
+---@param pred fun(value: T): boolean?
+---@return T[]
+function M.filter(it, pred)
+   local res = {}
+   for _, v in ipairs(it) do
+      if pred(v) then
+         table.insert(res, v)
+      end
+   end
+   return res
 end
 
 return M
--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,61 +1,64 @@
 local wezterm = require("wezterm")
+
 local helpers = require("helpers")
 
-local config = wezterm.config_builder()
+return helpers.doto(wezterm.config_builder(), function(config)
+   config.term = "wezterm"
 
-config.term = "wezterm"
+   -- Standard ricing
+   config.color_scheme = "nord"
 
--- Standard ricing
-config.color_scheme = "nord"
+   -- Font config
+   config.font = wezterm.font_with_fallback({ "TX02 Nerd Font Mono", "TX-02", "monospace" })
+   config.font_size = helpers.px_to_pt(14)
+   config.freetype_load_target = "Light"
+   config.freetype_render_target = "Light"
+   config.command_palette_font_size = config.font_size
+   config.underline_thickness = 1
 
--- Font config
-config.font = wezterm.font("TX-02")
-config.font_size = helpers.px_to_pt(14)
-config.command_palette_font_size = config.font_size
-config.underline_thickness = 1
 
-config.freetype_load_flags = "NO_BITMAP|FORCE_AUTOHINT"
-config.freetype_load_target = "Light"
-config.freetype_render_target = "HorizontalLcd"
+   -- Tab bar config
+   config.enable_tab_bar = true
+   config.use_fancy_tab_bar = false
+   config.tab_bar_at_bottom = true
+   config.hide_tab_bar_if_only_one_tab = true
 
--- Adjusting line height breaks AA on windows
-if not helpers.is_windows then
-   config.line_height = 1.05
-end
+   -- Scroll config
+   config.scrollback_lines = 25000
+   config.enable_scroll_bar = false
 
--- Tab bar config
-config.enable_tab_bar = true
-config.use_fancy_tab_bar = false
-config.tab_bar_at_bottom = true
-config.hide_tab_bar_if_only_one_tab = true
+   -- Bell feedback config
+   config.audible_bell = "Disabled"
 
--- Scroll config
-config.scrollback_lines = 25000
-config.enable_scroll_bar = false
+   -- Perf tuning
+   config.animation_fps = 1
+   config.cursor_blink_ease_in = "Constant"
+   config.cursor_blink_ease_out = "Constant"
 
--- Perf tuning
-config.max_fps = helpers.fps
-config.front_end = "OpenGL"
-config.animation_fps = 1
-config.cursor_blink_ease_in = "Constant"
-config.cursor_blink_ease_out = "Constant"
+   -- Window config
+   config.window_frame = { font = config.font, font_size = config.font_size }
+   config.window_padding = { left = 2, right = 2, top = 2, bottom = 2 }
+   config.adjust_window_size_when_changing_font_size = false
 
--- Bell feedback config
-config.audible_bell = "Disabled"
-
--- Window config
-config.window_frame = { font = config.font, font_size = config.font_size }
-config.window_padding = { left = 2, right = 2, top = 2, bottom = 2 }
-
--- Launch into WSL by default on Windows
-if helpers.is_windows then
-   config.wsl_domains = wezterm.default_wsl_domains()
-   if config.wsl_domains[1] ~= nil then
-      config.default_domain = config.wsl_domains[1].name
+   -- Launch into WSL by default on Windows
+   if helpers.running_on_windows() then
+      config.wsl_domains = wezterm.default_wsl_domains()
+      if config.wsl_domains[1] ~= nil then
+         config.default_domain = config.wsl_domains[1].name
+      end
    end
-end
 
--- Disable update checks, we have a package manager for that
-config.check_for_updates = false
+   -- Graphic config
+   if wezterm.gui and not helpers.running_in_vm() then
+      for _, gpu in ipairs(wezterm.gui.enumerate_gpus()) do
+         if gpu.device_type ~= "Cpu" and gpu.backend ~= "Gl" then
+            config.webgpu_preferred_adapter = gpu
+            config.front_end = "WebGpu"
+            break
+         end
+      end
+   end
 
-return config
+   -- Disable update checks, we have a package manager for that
+   config.check_for_updates = false
+end)
