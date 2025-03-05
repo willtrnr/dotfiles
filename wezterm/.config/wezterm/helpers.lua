98f7457e0002eb602562704634e6d9669521bdda
William Turner <willtur.will@gmail.com>
2025-03-04 19:40:01 -0500

    Fix pt math

--- wezterm/.config/wezterm/helpers.lua
+++ wezterm/.config/wezterm/helpers.lua
@@ -13,7 +13,7 @@ M.is_windows = not not wezterm.target_triple:find("-windows-")
 ---@param px number
 ---@return number
 function M.px_to_pt(px)
-   return (px / M.dpi) * (1 / 72)
+   return (px / M.dpi) / (1 / 72)
 end
 
 return M
