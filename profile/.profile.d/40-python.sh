387d29299ec4c00155df5e6adfbbce654bb584a1
William Turner <william.turner@aero.bombardier.com>
2025-04-02 14:13:42 -0400

    Add uv link mode config when home is on btrfs

--- profile/.profile.d/40-python.sh
+++ profile/.profile.d/40-python.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env sh
 
+if [ "$(stat -f -c %T "$HOME" 2>/dev/null)" = btrfs ]; then
+  # hardlink fails across subvols, but btrfs will reflink with copy
+  export UV_LINK_MODE=copy
+fi
+
 if command -v pyenv >/dev/null; then
   export PYENV_ROOT="$(pyenv root)"
   export PATH="${PYENV_ROOT}/shims:${PYENV_ROOT}/bin${PATH:+:${PATH}}"
