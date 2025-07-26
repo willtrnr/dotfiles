85eaf39f9fee80d871de4d50e1ae100804468739
William Turner <willtur.will@gmail.com>
2025-07-26 19:34:10 -0400

    Updated aconfmgr ignores

--- aconfmgr/.config/aconfmgr/10-ignores.sh
+++ aconfmgr/.config/aconfmgr/10-ignores.sh
@@ -1,36 +1,80 @@
-IgnorePath '*-'
-IgnorePath '*.backup'
-IgnorePath '*.bak'
-IgnorePath '*.cache'
-IgnorePath '*.lock'
-IgnorePath '*.orig'
-IgnorePath '*.pacnew'
-IgnorePath '*.pacsave'
-IgnorePath '*.updated'
-IgnorePath '/.snapshots'
-IgnorePath '/boot'
-IgnorePath '/centos'
-IgnorePath '/efi'
-IgnorePath '/etc/adjtime'
-IgnorePath '/etc/ca-certificates'
-IgnorePath '/etc/cups'
-IgnorePath '/etc/fonts'
-IgnorePath '/etc/lvm'
-IgnorePath '/etc/pacman.d/gnupg'
-IgnorePath '/etc/ssh/ssh_host_*'
-IgnorePath '/etc/ssl'
-IgnorePath '/etc/texmf'
+# FHS local only mount-points
 IgnorePath '/media'
 IgnorePath '/mnt'
+
+# General local state
 IgnorePath '/opt'
-IgnorePath '/rhel'
-IgnorePath '/usr/lib'
-IgnorePath '/usr/local'
-IgnorePath '/usr/share'
-IgnorePath '/var/db/sudo'
-IgnorePath '/var/lib'
-IgnorePath '/var/log'
-IgnorePath '/var/opt'
-IgnorePath '/var/run'
-IgnorePath '/var/spool'
-IgnorePath '/var/tmp'
+IgnorePath '/tmp'
+IgnorePath '/var'
+
+# Local accounts and related
+for f in \
+  group \
+  gshadow \
+  passwd \
+  shadow \
+  shells \
+  subgid \
+  subuid \
+; do
+  IgnorePath "/etc/$f"
+  IgnorePath "/etc/$f-"
+done
+
+# Machine identity stuff
+IgnorePath '/etc/hostname'
+IgnorePath '/etc/hosts'
+IgnorePath '/etc/machine-id'
+IgnorePath '/etc/os-release'
+
+# Pacman stuff
+IgnorePath '*.pacnew'
+IgnorePath '*.pacsave'
+IgnorePath '/etc/pacman.d/*-backup'
+IgnorePath '/etc/pacman.d/cachyos-*mirrorlist'
+IgnorePath '/etc/pacman.d/gnupg'
+IgnorePath '/etc/pacman.d/mirrorlist'
+
+# Kernel modules
+IgnorePath '/usr/lib/modules'
+
+# Generated/false-positive files
+IgnorePath '/etc/ca-certificates/extracted'
+IgnorePath '/etc/ld.so.cache'
+IgnorePath '/etc/ssl/certs'
+IgnorePath '/etc/udev/hwdb.bin'
+IgnorePath '/etc/vconsole.conf'
+IgnorePath '/usr/bin/groupmems'
+IgnorePath '/usr/bin/nopt'
+IgnorePath '/usr/lib/gconv/gconv-modules.cache'
+IgnorePath '/usr/lib/locale/locale-archive'
+IgnorePath '/usr/lib/udev/hwdb.bin'
+IgnorePath '/usr/lib/utempter/utempter'
+
+# Local mount units
+IgnorePath '/etc/systemd/system/*.mount'
+IgnorePath '/etc/systemd/system/*.target/*.mount'
+
+# Random temp files
+IgnorePath '/etc/*.OLD'
+IgnorePath '/etc/.pwd.lock'
+IgnorePath '/etc/.updated'
+
+# btrfs snapshots
+IgnorePath '/.snapshots'
+
+# Python stuff
+IgnorePath '*/__pycache__/*'
+IgnorePath '/usr/lib/python*/site-packages'
+
+# ext4 garbage
+IgnorePath '/lost+found'
+
+# WSL injected files
+IgnorePath '/etc/ld.so.conf.d/ld.wsl.conf'
+IgnorePath '/etc/wsl-distribution.conf'
+IgnorePath '/init'
+IgnorePath '/usr/bin/mount.drvfs'
+IgnorePath '/usr/bin/wslinfo'
+IgnorePath '/usr/bin/wslpath'
+IgnorePath '/usr/lib/wsl/*'
