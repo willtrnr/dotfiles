2b955e4b1f7d0242af4629a68b269a7cc0af0716
William Turner <william.turner@aero.bombardier.com>
2025-12-10 14:47:43 -0500

    Use WSL native with cargo-xwin

--- profile/.profile.d/40-rust.sh
+++ profile/.profile.d/40-rust.sh
@@ -4,6 +4,13 @@ export RUSTUP_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/rustup"
 
 export PATH="${HOME}/.cargo/bin${PATH:+:${PATH}}"
 
-if SCCACHE="$(command -v sccache)"; then
-  export RUSTC_WRAPPER="$SCCACHE"
+if RUSTC_WRAPPER="$(command -v sccache)"; then
+  export RUSTC_WRAPPER
+fi
+
+if [ -n "$WSL_DISTRO_NAME" ]; then
+  export CARGO_TARGET_I686_PC_WINDOWS_GNU_RUNNER=/usr/bin/env
+  export CARGO_TARGET_I686_PC_WINDOWS_MSVC_RUNNER=/usr/bin/env
+  export CARGO_TARGET_X86_64_PC_WINDOWS_GNU_RUNNER=/usr/bin/env
+  export CARGO_TARGET_X86_64_PC_WINDOWS_MSVC_RUNNER=/usr/bin/env
 fi
