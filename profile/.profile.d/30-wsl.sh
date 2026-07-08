0b5022e6de6b05c9405d934ac78aa16a6d379245
William Turner <william.turner@aero.bombardier.com>
2026-07-08 12:24:41 -0400

    Add some fallback for WSL detection and remove wezterm unit

--- profile/.profile.d/30-wsl.sh
+++ profile/.profile.d/30-wsl.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env sh
 
+if [ -z "${WSL_DISTRO_NAME}" ] && command -v wslpath >/dev/null; then
+  case "$(uname -r)" in
+    *WSL2)
+      if WSL_DISTRO_NAME="$(wslpath -a -m / 2>/dev/null | cut -d/ -f4)"; then
+        export WSL_DISTRO_NAME
+      fi
+      ;;
+  esac
+fi
+
 if [ -n "${WSL_DISTRO_NAME}" ] && [ -z "${DISPLAY}" ]; then
   export DISPLAY=:0
 fi
