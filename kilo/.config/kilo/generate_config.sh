a3753a77555ef0800e5a259105c40fdeb755db99
William Turner <willtur.will@gmail.com>
2026-09-05 17:23:45 -0400

    Rework AI stuff

--- kilo/.config/kilo/generate_config.sh
+++ kilo/.config/kilo/generate_config.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/env sh
 
-jq --argjson models "$(curl -q http://localhost:12434/v1/models)" \
-  '.provider.local.models = add(
-    $models | .data[] | select(.capabilities.function_calling) | {
-      (.id): {
-        name: .id | split("/") | .[-1] | gsub("[\\s_-]+"; " "),
-        tool_call: true,
-        reasoning: true,
-        modalities: {
-          input: .architecture.input_modalities,
-          output: .architecture.output_modalities,
-        },
-        limit: {
-          context: .context_length,
-          output: 16384,
-        },
-      },
-    }
-  )' \
-  "$(dirname -- $(readlink -f -- "$0"))/kilo.jsonc"
+jq --argjson models "$(curl -q http://localhost:12434/v1/models)" '.
+  | .provider.local.models = add(
+      $models | .data[] | select(.meta.llamaswap.embedding // false | not) | {
+        (.id): {
+          name: .id | split("/") | .[-1] | gsub("[\\s_-]+"; " "),
+          tool_call: .capabilities.function_calling,
+          reasoning: .meta.llamaswap.reasoning,
+          modalities: {
+            input: .architecture.input_modalities,
+            output: .architecture.output_modalities,
+          },
+        } * (.meta.llamaswap.kilo // {}),
+      }
+    )
+  | .indexing |= . * (
+      $models | .data[] | select(.meta.llamaswap.embedding // false) | {
+        model: .id,
+      } * (.meta.llamaswap.kilo // {})
+    )
+' "$(dirname -- $(readlink -f -- "$0"))/kilo.jsonc"
